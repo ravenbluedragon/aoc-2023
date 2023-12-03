43e4faff22a4de70b1b27cb10920ee6217752cbf
Raven-Blue Dragon <4637537+ravenbluedragon@users.noreply.github.com>
2023-12-03 18:13:20 +1100

    Refactor code to use grid.Point2D for positions

--- 03/main_test.go
+++ 03/main_test.go
@@ -2,6 +2,8 @@ package main
 
 import (
 	"testing"
+
+	"github.com/ravenbluedragon/aoc-2023/common/grid"
 )
 
 const base_data = "../test-data/"
@@ -39,26 +41,13 @@ var expectedNumbers []PartNumber = []PartNumber{
 	{9, 5, 7, 598},
 }
 
-/*
-467..114..
-...*......
-..35..633.
-......#...
-617*......
-.....+.58.
-..592.....
-......755.
-...$.*....
-.664.598..
-*/
-
-var exepectedSymbols map[[2]int]rune = map[[2]int]rune{
-	{1, 3}: '*',
-	{3, 6}: '#',
-	{4, 3}: '*',
-	{5, 5}: '+',
-	{8, 3}: '$',
-	{8, 5}: '*',
+var exepectedSymbols map[grid.Point2D]rune = map[grid.Point2D]rune{
+	grid.P2(3, 1): '*',
+	grid.P2(6, 3): '#',
+	grid.P2(3, 4): '*',
+	grid.P2(5, 5): '+',
+	grid.P2(3, 8): '$',
+	grid.P2(5, 8): '*',
 }
 
 func TestLoadNumbersAndSymbols(t *testing.T) {
@@ -86,6 +75,30 @@ func TestBorder(t *testing.T) {
 	for i, n := range expectedNumbers {
 		if n.BordersSymbol(exepectedSymbols) != intersection[i] {
 			t.Errorf("numbers[%d].BordersSymbol() = %v, expected %v", i, n.BordersSymbol(exepectedSymbols), intersection[i])
+		}
+	}
+}
+
+func TestAdjacent(t *testing.T) {
+	table := []struct {
+		pos      grid.Point2D
+		expected []int
+	}{
+		{grid.P2(3, 1), []int{0, 2}},
+		{grid.P2(3, 4), []int{4}},
+		{grid.P2(5, 8), []int{7, 9}},
+	}
+
+	for _, test := range table {
+		actual := adjacent(test.pos, expectedNumbers)
+		if len(actual) != len(test.expected) {
+			t.Errorf("adjacent(%v) = %v, expected indexes %v", test.pos, actual, test.expected)
+		}
+		for i, n := range actual {
+			expected := expectedNumbers[test.expected[i]]
+			if n != expected {
+				t.Errorf("adjacent(%v)[%d] = %v, expected %v", test.pos, i, n, expected)
+			}
 		}
 	}
 }
