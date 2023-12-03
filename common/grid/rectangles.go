a5de177478696d5d4e59978ef7d88d7f09d94a3f
Raven-Blue Dragon <4637537+ravenbluedragon@users.noreply.github.com>
2023-12-03 23:54:29 +1100

    Refactor day3 border calculation and add rectangle utility methods

--- common/grid/rectangles.go
+++ common/grid/rectangles.go
@@ -20,6 +20,20 @@ func R2dim(x, y, w, h int) Rect2D {
 	return Rect2D{Point2D{x, y}, Point2D{x + w, y + h}}
 }
 
+// Contains returns true if the rectangle contains the given point
+func (r Rect2D) Contains(p Point2D) bool {
+	return r.Min.X <= p.X && p.X <= r.Max.X &&
+		r.Min.Y <= p.Y && p.Y <= r.Max.Y
+}
+
+// Grow8 returns a rectangle grown by the given size in all directions
+func (r Rect2D) Grow8(size int) Rect2D {
+	return Rect2D{
+		Point2D{r.Min.X - size, r.Min.Y - size},
+		Point2D{r.Max.X + size, r.Max.Y + size},
+	}
+}
+
 // Rect3D represents a rectangle in 3D space
 type Rect3D struct {
 	Min, Max Point3D
@@ -38,4 +52,19 @@ func R3c(x1, y1, z1, x2, y2, z2 int) Rect3D {
 // R3dim returns a 3D rectangle from coordinates and dimensions
 func R3dim(x, y, z, w, h, d int) Rect3D {
 	return Rect3D{Point3D{x, y, z}, Point3D{x + w, y + h, z + d}}
+}
+
+// Contains returns true if the rectangle contains the given point
+func (r Rect3D) Contains(p Point3D) bool {
+	return r.Min.X <= p.X && p.X <= r.Max.X &&
+		r.Min.Y <= p.Y && p.Y <= r.Max.Y &&
+		r.Min.Z <= p.Z && p.Z <= r.Max.Z
+}
+
+// Grow12 returns a rectangle grown by the given size in all directions
+func (r Rect3D) Grow12(size int) Rect3D {
+	return Rect3D{
+		Point3D{r.Min.X - size, r.Min.Y - size, r.Min.Z - size},
+		Point3D{r.Max.X + size, r.Max.Y + size, r.Max.Z + size},
+	}
 }
