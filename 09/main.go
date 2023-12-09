a34d519654c5302a019fdc6192df17ea69949270
Raven-Blue Dragon <4637537+ravenbluedragon@users.noreply.github.com>
2023-12-09 16:31:01 +1100

    Solution for Day 09

---
+++ 09/main.go
@@ -0,0 +1,96 @@
+package main
+
+import (
+	"log"
+
+	"github.com/ravenbluedragon/aoc-2023/common"
+	"github.com/ravenbluedragon/aoc-2023/common/parse"
+)
+
+// boilerplate to load and solve puzzles
+func main() {
+	common.ChoosePart(9, solve1, solve2)
+}
+
+func solve1(filename string) any {
+	lines := common.LoadData(filename)
+	data := parseInput(lines)
+	sum := 0
+	for _, seq := range data {
+		sum += extrapolateNext(seq)
+	}
+	return sum
+}
+
+func solve2(filename string) any {
+	lines := common.LoadData(filename)
+	data := parseInput(lines)
+	sum := 0
+	for _, seq := range data {
+		sum += extrapolatePrev(seq)
+	}
+	return sum
+}
+
+func parseInput(lines []string) [][]int {
+	var result [][]int
+	for _, line := range lines {
+		ints, err := parse.StringToIntList(line, " ")
+		if err != nil {
+			log.Fatalf("Failed to parse line: %s", line)
+		}
+		result = append(result, ints)
+	}
+	return result
+}
+
+func allZero(seq []int) bool {
+	for _, i := range seq {
+		if i != 0 {
+			return false
+		}
+	}
+	return true
+}
+
+func extrapolateNext(seq []int) int {
+	var sequences [][]int
+	sequences = append(sequences, seq)
+	prev := seq
+	for !allZero(prev) {
+		current := make([]int, len(prev)-1)
+		for i, v := range prev[1:] {
+			current[i] = v - prev[i]
+		}
+		sequences = append(sequences, current)
+		prev = current
+	}
+	diff := 0
+	for i := len(sequences) - 1; i >= 0; i-- {
+		diff += last(sequences[i])
+	}
+	return diff
+}
+
+func last(seq []int) int {
+	return seq[len(seq)-1]
+}
+
+func extrapolatePrev(seq []int) int {
+	var sequences [][]int
+	sequences = append(sequences, seq)
+	prev := seq
+	for !allZero(prev) {
+		current := make([]int, len(prev)-1)
+		for i, v := range prev[1:] {
+			current[i] = v - prev[i]
+		}
+		sequences = append(sequences, current)
+		prev = current
+	}
+	diff := 0
+	for i := len(sequences) - 1; i >= 0; i-- {
+		diff = sequences[i][0] - diff
+	}
+	return diff
+}
